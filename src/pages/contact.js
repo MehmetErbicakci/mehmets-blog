795f7c576df119e06e2bf2e52314e46322281f39
MehmetErbicakci <83139833+MehmetErbicakci@users.noreply.github.com>
2022-02-21 00:34:51 +0300

    qweqwe

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,13 +11,13 @@ const ContactPage = ({
   return (
     <Layout>
       <Helmet>
-        <title>Contact — {site.siteMetadata.title}</title>
+        <title>İletişim — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
+          <h1 className="post-title">İletişim</h1>
+          <p>Bana Ulaşmak İçin Formu Doldur! &rarr;</p>
         </div>
         <div>
           <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
